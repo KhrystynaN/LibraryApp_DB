c0641db9e90a7a0471c9ce9badac5cacd009f7f6
KhrystynaN <chrystylka@gmail.com>
2023-03-24 12:49:41 -0400

    login scenario and step def is done2

--- src/test/java/com/cydeo/steps/LoginStepDefs.java
+++ src/test/java/com/cydeo/steps/LoginStepDefs.java
@@ -12,11 +12,13 @@ public class LoginStepDefs {
     LoginPage loginPage = new LoginPage();
     DashBoardPage dashBoardPage = new DashBoardPage();
     String actualUserName;
+    String email;
 
     @Given("the user logged in  {string} and {string}")
     public void the_user_logged_in_and(String email, String password) {
        loginPage.login(email,password);
        BrowserUtil.waitFor(2);
+       this.email=email;
     }
 
     @When("user gets username  from user fields")
@@ -28,7 +30,7 @@ public class LoginStepDefs {
     public void the_username_should_be_same_with_database() {
         //get data from database
 
-        String query = "select full_name from users where email = 'librarian56@library'";
+        String query = "select full_name from users where email = '"+ email +"'";
 
         DB_Util.runQuery(query);
 
